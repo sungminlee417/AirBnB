eb51891bb8e9f4c60407ebe654bbcb36ec32aaab
Sungmin Lee <sungminlee417@gmail.com>
2022-07-12 21:53:48 -0700

    redo

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -8,24 +8,7 @@ const { handleValidationErrors } = require("../../utils/validation");
 
 const router = express.Router();
 
-const validateSignup = [
-  check("email")
-    .exists({ checkFalsy: true })
-    .isEmail()
-    .withMessage("Please provide a valid email."),
-  check("username")
-    .exists({ checkFalsy: true })
-    .isLength({ min: 4 })
-    .withMessage("Please provide a username with at least 4 characters."),
-  check("username").not().isEmail().withMessage("Username cannot be an email."),
-  check("password")
-    .exists({ checkFalsy: true })
-    .isLength({ min: 6 })
-    .withMessage("Password must be 6 characters or more."),
-  handleValidationErrors,
-];
-
-router.post("/", validateSignup, async (req, res) => {
+router.post("/", async (req, res) => {
   const { email, password, username } = req.body;
   const user = await User.signup({ email, username, password });
 
