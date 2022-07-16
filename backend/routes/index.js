5edf589c8f85d094b22d13fc2e7155a44e4d16c0
Sungmin Lee <sungminlee417@gmail.com>
2022-07-16 11:23:11 -0700

    done testing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -15,10 +15,6 @@ router.use("/api", apiRouter);
 router.use("/me", currentUserRouter);
 router.use("/spots", spotsRouter);
 
-router.get("/", (req, res) => {
-  res.json("hello");
-});
-
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
